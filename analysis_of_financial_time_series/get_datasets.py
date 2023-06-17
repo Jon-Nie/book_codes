a60267febadc2f95fc1649688f55d5310b4f98ea
Jon-Nie <86107014+Jon-Nie@users.noreply.github.com>
2023-06-18 00:08:12 +0200

    refactor into function

--- analysis_of_financial_time_series/get_datasets.py
+++ analysis_of_financial_time_series/get_datasets.py
@@ -1,31 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
-import html
+from html import unescape
 import os
 import urllib.request
 
-base_url = 'https://faculty.chicagobooth.edu/'
-url = r'https://faculty.chicagobooth.edu/ruey-s-tsay/research/analysis-of-financial-time-series-3rd-edition'
-string = requests.get(url).text
+def download_datasets() -> None:
+	base_url = "https://faculty.chicagobooth.edu/"
+	url = r"https://faculty.chicagobooth.edu/ruey-s-tsay/research/analysis-of-financial-time-series-3rd-edition"
+	html = requests.get(url).text
+	chapters = ["<h2>" + chapter for chapter in html.split("<h2>")][1:]
 
-delimiter_h2 = '<h2>'
-string = [delimiter_h2 + sub_string for sub_string in string.split(delimiter_h2)][1:]
+	for chapter in chapters:
+		chapter = unescape(chapter)
+		soup = BeautifulSoup(chapter, "lxml")
+		title = soup.find("h2").text.replace(":", "").replace("Chapter", "").lstrip().strip("\n")
+		print(title)
 
-for chapter in string:
-	chapter = html.unescape(chapter)
-	soup = BeautifulSoup(chapter, "lxml")
-	chapter_title = soup.find('h2').text.replace(':', '').replace('Chapter', '').lstrip().strip('\n')
-	print("\n", chapter_title,"\n")
-	if not os.path.exists(chapter_title):
-		os.makedirs(f"{chapter_title}/datasets")
-	rows = soup.find_all('a')
-	for row in rows:
-		name = row.text
-		href = row.get('href')
-		file_link = base_url + href
-		if 'C:/Users' in file_link:
-			continue
-		if href == "https://www.chicagobooth.edu/":
-			break
-		print("\t", file_link)
-		urllib.request.urlretrieve(file_link, f"{chapter_title}/datasets/{name}")
+		if not os.path.exists(title):
+			os.makedirs(f"{title}/datasets")
+
+		for row in soup.find_all("a"):
+			name = row.text
+			href = row.get("href")
+			url = f"{base_url}{href}"
+			if "C:/Users" in url:
+				continue
+			if href == "https://www.chicagobooth.edu/":
+				break
+			try:
+				urllib.request.urlretrieve(url, f"{title}/datasets/{name}")
+				print(f"   {url}")
+			except urllib.error.HTTPError:
+				print(f"   failed: {url}")
+
+if __name__ == "__main__":
+	download_datasets()
